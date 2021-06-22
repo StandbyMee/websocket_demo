68ae935351f10e30666fff2e8777bdd5f788a2db
Yiran <1011700859@qq.com>
2021-06-22 17:43:47 +0800

    Second Commit. Completed WebSocketStompConfig.java

--- src/main/java/com/practice/websocket_demo/websocket/WebSocketStompConfig.java
+++ src/main/java/com/practice/websocket_demo/websocket/WebSocketStompConfig.java
@@ -1,4 +1,14 @@
 package com.practice.websocket_demo.websocket;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+//Configuration是一个类级别的注解，指明对象时bean定义的来源
 public class WebSocketStompConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
